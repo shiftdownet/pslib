751f84647d7a5c9cb85e32b77aeb5514794ff141
Hiroki Nakayama <k11096kk@gmail.com>
2023-07-29 11:37:51 +0900

    wow

---
+++ table/TableMerger.psm1
@@ -0,0 +1,53 @@
+﻿
+class IMergeableTable {
+    [PsCustomObject[]]$table = $null
+
+    [String]getPrimaryKey(){
+        throw "Not implemented error"
+    }
+}
+
+class IMergeCondition {
+    [bool]canMerge( [PsCustomObject]$src, [PsCustomObject]$dst ){
+        throw "Not implemented error"
+    }
+}
+
+class TableMerger {
+    [IMergeableTable]$src = $null
+    [IMergeableTable]$dst = $null
+    [PsCustomObject[]]$merged = @()
+    [IMergeCondition]$condition = $null
+
+    TableMerger( [IMergeableTable]$src, [IMergeableTable]$dst, [IMergeCondition]$mergeCondition ){
+        $this.src = $src
+        $this.dst = $dst
+        $this.condition = $mergeCondition
+    }
+
+    [void]merge(){
+        # 処理時間をO(n^2)からO(n)にするためにインデックス作成
+        $srcIndex = $this.createIndex($this.src)
+        $dstIndex = $this.createIndex($this.dst)
+
+        foreach( $record in $dstIndex.psobject.properties ) {
+            $key = $record.name
+            $value = $record.value
+            # dstRecordのプライマリーキーがsrcにも存在している
+            if ( $null -ne $srcIndex.$key ) {
+                if ( $this.condition.canMerge( $srcIndex.$key, $value ) ){
+                    $this.condition.merge( $srcIndex.$key, $value )
+                }
+            }
+            $this.merged += $value
+        }
+    }
+
+    [PsCustomObject]createIndex($iMergeableTable){
+        $index = [PsCustomObject]@{}
+        foreach( $record in $iMergeableTable.table ){
+            $index | Add-Member -MemberType NoteProperty -Name $iMergeableTable.getPrimaryKey( $record ) -Value $record
+        }
+        return $index
+    }
+}
---
+++ test/Test_TableMerger.ps1
@@ -0,0 +1,28 @@
+﻿
+
+using module ".\..\table\TableMerger.psm1"
+
+class QACTable : IMergeableTable {
+    [String]getPrimaryKey( $record ){
+        return $record.ID + $record.ErrorMessage
+    }
+}
+
+class QACMerge:IMergeCondition {
+    [bool]canMerge( [PsCustomObject]$src, [PsCustomObject]$dst ){
+        return $dst.Comment -eq ""
+    }
+    [void]merge([PsCustomObject]$src, [PsCustomObject]$dst){
+        $dst.Comment = $src.Comment
+    }
+}
+
+$before = [QACTable]::new()
+$before.table = Import-Csv "./sample_data_rev1.csv"
+$after = [QACTable]::new()
+$after.table = Import-Csv "./sample_data_rev2.csv"
+
+$tableMerger = [TableMerger]::new( $before, $after, [QACMerge]::new() )
+$tableMerger.merge()
+
+$tableMerger.merged | Export-CSV -Path "./merged.csv" -NoTypeInformation
